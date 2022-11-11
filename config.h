d85825486242e80a487fb6cbb4dd241704e42002
Andrew Wang <andrewwang298@gmail.com>
2022-11-11 16:47:02 +0000

    Change st modkey to windows

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=20:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=20:antialias=true:autohint=true" };
+/* static char *font = "mono:pixelsize=20:antialias=true:autohint=true"; */
+static char *font = "SourceCodePro Light:size=20:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=20:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 /*
@@ -107,7 +108,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -259,11 +260,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ Mod4Mask,             XK_c,           clipcopy,       {.i =  0} },
+	{ Mod1Mask,          	XK_c,           clipcopy,       {.i =  0} },
+	{ Mod1Mask,          	XK_v,           clippaste,      {.i =  0} },
 	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
-	{ Mod4Mask,             XK_v,           clippaste,      {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
@@ -282,8 +281,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
-	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
-	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
+	/* { TERMMOD,              XK_U,           zoom,           {.f = +2} }, */
+	/* { TERMMOD,              XK_D,           zoom,           {.f = -2} }, */
 	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
